6f9a9cc978db22c869be42c6ba3070a8bc230c51
dojsys <div.ositaj@gmail.com>
2019-09-13 22:53:05 +0800

    update gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,6 +8,54 @@ exports.createPages = async ({ graphql, actions }) => {
   const result = await graphql(
     `
       {
+        allMdx {
+          edges {
+            node {
+              id
+              tableOfContents
+              timeToRead
+              frontmatter {
+                slug
+                title
+                description
+                date
+                type
+                featured
+                cover {
+                  childImageSharp {
+                    fluid(
+                      maxWidth: 1500
+                      maxHeight: 700
+                      quality: 100
+                      cropFocus: ENTROPY
+                    ) {
+                      base64
+                      tracedSVG
+                      aspectRatio
+                      src
+                      srcSet
+                      srcWebp
+                      srcSetWebp
+                      sizes
+                      originalImg
+                      originalName
+                      presentationWidth
+                      presentationHeight
+                    }
+                  }
+                }
+              }
+              parent {
+                ... on File {
+                  sourceInstanceName
+                  absolutePath
+                  relativePath
+                  name
+                }
+              }
+            }
+          }
+        }
         allMarkdownRemark(
           sort: { fields: [frontmatter___date], order: DESC }
           limit: 1000
@@ -32,22 +80,33 @@ exports.createPages = async ({ graphql, actions }) => {
   }
 
   // Create blog posts pages.
-  const posts = result.data.allMarkdownRemark.edges
-
-  posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    const next = index === 0 ? null : posts[index - 1].node
-
+  result.data.allMdx.edges.forEach(({ node }) => {
     createPage({
-      path: post.node.fields.slug,
-      component: blogPost,
+      path: `/posts/${node.frontmatter.slug}`,
+      component: node.parent.absolutePath,
       context: {
-        slug: post.node.fields.slug,
-        previous,
-        next,
+        absPath: node.parent.absolutePath,
+        timeToRead: node.timeToRead,
+        cover: node.frontmatter.cover,
+        tableOfContents: node.tableOfContents,
       },
-    })
-  })
+    });
+  });
+  
+//   const posts = result.data.allMarkdownRemark.edges
+//   posts.forEach((post, index) => {
+//     const previous = index === posts.length - 1 ? null : posts[index + 1].node
+//     const next = index === 0 ? null : posts[index - 1].node
+//     createPage({
+//       path: post.node.fields.slug,
+//       component: blogPost,
+//       context: {
+//         slug: post.node.fields.slug,
+//         previous,
+//         next,
+//       },
+//     })
+//   })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
