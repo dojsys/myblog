b3062c57b859f337bc2371bd95f8e395312d4d70
dojsys <div.ositaj@gmail.com>
2019-09-14 00:07:56 +0800

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,104 +2,93 @@ const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+    const { createPage } = actions;
 
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  return new Promise((resolve, reject) => {
-    resolve(
-      graphql(
-    `
-      {
-        allMdx {
-          edges {
-            node {
-              id
-              tableOfContents
-              timeToRead
-              frontmatter {
-                slug
-                title
-                description
-                date
-                type
-                featured
-                cover {
-                  childImageSharp {
-                    fluid(
-                      maxWidth: 1500
-                      maxHeight: 700
-                      quality: 100
-                      cropFocus: ENTROPY
-                    ) {
-                      base64
-                      tracedSVG
-                      aspectRatio
-                      src
-                      srcSet
-                      srcWebp
-                      srcSetWebp
-                      sizes
-                      originalImg
-                      originalName
-                      presentationWidth
-                      presentationHeight
+    const blogPost = path.resolve(`./src/templates/blog-post.js`);
+  
+    return new Promise((resolve, reject) => {
+        resolve(
+            graphql(
+                `
+                    {
+                        allMdx {
+                        edges {
+                            node {
+                            id
+                            tableOfContents
+                            timeToRead
+                            frontmatter {
+                                slug
+                                title
+                                description
+                                date
+                                type
+                                featured
+                                cover {
+                                childImageSharp {
+                                    fluid(
+                                    maxWidth: 1500
+                                    maxHeight: 700
+                                    quality: 100
+                                    cropFocus: ENTROPY
+                                    ) {
+                                    base64
+                                    tracedSVG
+                                    aspectRatio
+                                    src
+                                    srcSet
+                                    srcWebp
+                                    srcSetWebp
+                                    sizes
+                                    originalImg
+                                    originalName
+                                    presentationWidth
+                                    presentationHeight
+                                    }
+                                }
+                                }
+                            }
+                            parent {
+                                ... on File {
+                                sourceInstanceName
+                                absolutePath
+                                relativePath
+                                name
+                                }
+                            }
+                            }
+                        }
+                        }
                     }
-                  }
+                `
+            ).then(result => {
+                if (result.errors) {
+                    reject(result.errors);
                 }
-              }
-              parent {
-                ... on File {
-                  sourceInstanceName
-                  absolutePath
-                  relativePath
-                  name
-                }
-              }
-            }
-          }
-        }
-      }
-    `
-  ).then(result => {
-        if (result.errors) {
-          reject(result.errors);
-        }
 
-        // Create blog posts pages.
-        const posts = result.data.allMdx.edges;
-        posts.forEach(({ post, index }) => {
-          const previous = index === posts.length - 1 ? null : posts[index + 1].node
-          const next = index === 0 ? null : posts[index - 1].node
-          createPage({
-            path: `/posts/${post.node.frontmatter.slug}`,
-            component: post.node.parent.absolutePath,
-            //path: node.fields.slug,
-            //component: blogPost,
-            context: {
-              absPath: post.node.parent.absolutePath,
-              timeToRead: post.node.timeToRead,
-              cover: post.node.frontmatter.cover,
-              tableOfContents: post.node.tableOfContents,
-              previous,
-              next,
-            },
-          });
-        });
-        
-      })
-    );
-  });
-};
+                // Create blog posts pages.
+                const posts = result.data.allMarkdownRemark.edges;
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+                result.data.allMdx.edges.forEach(({ node, index }) => {
+                    const previous = index === posts.length - 1 ? null : posts[index + 1].node;
+                    const next = index === 0 ? null : posts[index - 1].node;
+                    createPage({
+                        path: `/posts/${node.frontmatter.slug}`,
+                        component: node.parent.absolutePath,
+                        context: {
+                            absPath: node.parent.absolutePath,
+                            timeToRead: node.timeToRead,
+                            cover: node.frontmatter.cover,
+                            tableOfContents: node.tableOfContents,
+                            previous,
+                            next,
+                        },
+                    });
+                });
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
+            })
+        );
+    });
 }
+
+
